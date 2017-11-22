2c426c064e297fb456d9074956502f8b6f03165a
Muhsin Ahadi <muhsin_ahadi@student.uns.ac.id>
2017-11-22 20:14:02 +0700

    Edit
    Edit background

--- app/src/main/java/informatika/poekwe/SplashScreen.java
+++ app/src/main/java/informatika/poekwe/SplashScreen.java
@@ -2,26 +2,28 @@ package informatika.poekwe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.content.Intent;
 
 public class SplashScreen extends AppCompatActivity {
 
 
-    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        new Handler().postDelayed(new Runnable() {
+        Thread myThread = new Thread(){
             @Override
             public void run() {
-                Intent i = new Intent (SplashScreen.this, MainActivity.class);
-            startActivity(i);
-
-            finish();
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }, SPLASH_TIME_OUT);
+        };
+        myThread.start();
     }
 }
